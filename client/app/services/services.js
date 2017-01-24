559f056c5e2b6eace4b5a597c77f33643f6033b8
Edmund Louie <ELouie@Edmunds-MacBook-Pro.local>
2017-01-23 18:59:07 -0800

    clickable playlist functionality

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -50,7 +50,7 @@ angular.module('rain.services', [])
           type: 'video',
           videoEmbeddable: true,
           key: 'AIzaSyBWzdeA8Kc4DD__k7IgNKTblq0dAMXm0xs',
-          q: queryStr[randomNum()] + ' song',
+          q: queryStr[randomNum()] + ' anime song',
           maxResults: 10
         }
       }).then(function(resp) {
