f27a98bc8a3e23705548091d4ef237daf84f1d14
Edmund Louie <ELouie@edmunds-mbp.attlocal.net>
2017-01-25 12:56:17 -0800

    implement grunt functionalities

---
+++ Gruntfile.js
@@ -0,0 +1,104 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['client/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+
+    clean: ['dist/<%= pkg.name %>.js'],
+
+    nodemon: {
+      dev: {
+        script: 'server/server.js'
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'dist/build.min.js': ['dist/<%= pkg.name %>.js']
+        }
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'client/styles',
+          src: ['*.css', '!*.min.css'],
+          dest: 'dist',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: [
+          'client/**/*.js',
+          'lib/**/*.js',
+        ],
+        tasks: [
+          'concat',
+          'uglify'
+        ]
+      },
+      css: {
+        files: 'client/styles/*.css',
+        tasks: ['cssmin']
+      }
+    },
+
+    shell: {
+      prodServer: {
+        command: 'git push live master',
+        options: {
+          stdout: true,
+          stderr: true,
+          failOnError: true
+        }
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('server-dev', function (target) {
+    grunt.task.run([ 'nodemon', 'watch' ]);
+  });
+
+  // Main grunt tasks
+
+  grunt.registerTask('build', [
+    'concat',
+    'uglify',
+    'clean',
+    'cssmin'
+  ]);
+
+  grunt.registerTask('upload', function(n) {
+    if (grunt.option('prod')) {
+      grunt.task.run([ 'shell:prodServer' ]);
+    } else {
+      grunt.task.run([ 'server-dev' ]);
+    }
+  });
+
+  grunt.registerTask('deploy', [
+    'build',
+    'upload'
+  ]);
+};
